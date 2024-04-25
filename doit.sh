b4ea8475abd9315bd765ebb435d1839e84f85f54
Pete Cheslock <petecheslock@gmail.com>
2024-04-25 10:40:14 -0400

    Update compose and run command to go from zero to waltz

--- doit.sh
+++ doit.sh
@@ -1,2 +1,18 @@
+# Using Versions:
+# Apache Maven 3.8.7 (b89d5959fcde851dcb1c8946a785a163f14e1e29)
+# Java version: 18.0.2.1, vendor: Oracle Corporation, runtime: /Users/petecheslock/.asdf/installs/java/oracle-18.0.2.1
+
+# start postgres
 docker-compose up -d postgres
+# Get db dump https://github.com/finos/waltz/blob/master/waltz-sample-data/database/pg/versions/postgres-dump-1.47.1.sql.gz
+pg_restore -p 5432 -U waltz -h localhost -d waltz < ~/Downloads/postgres-dump-1.47.1.sql.gz
+# bring schema up to date
+mvn -s .build.settings.xml compile -P waltz-postgres,dev-postgres -f waltz-schema/pom.xml -DskipTests=true
+# build package
+mvn -s .build.settings.xml clean package -P waltz-postgres,dev-postgres -DskipTests=true
+# run the jar with appmap enabled
 java -javaagent:$HOME/.appmap/lib/java/appmap.jar -jar ./waltz-web/target/waltz-web-jar-with-dependencies.jar org.finos.waltz.web.Main
+
+# Open waltz (admin/password)
+# NOTE: Can take 5-10 seconds to respond via web
+http://localhost:8443
